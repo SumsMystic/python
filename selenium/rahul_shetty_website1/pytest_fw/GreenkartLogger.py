8c2e3100479c2109bf698061290ab5091a0457f0
Sumeet Agrawal <slg.agra@gmail.com>
2024-05-12 17:18:13 +0530

    Ph2
    Ph2 of initial check-in

--- selenium/rahul_shetty_website1/pytest_fw/GreenkartLogger.py
+++ selenium/rahul_shetty_website1/pytest_fw/GreenkartLogger.py
@@ -12,8 +12,8 @@ class Greenkart_Logging:
     def __init__(self):
         pass
     
-    def get_logger(self):
-        self.proj_logger = logging.Logger("TEST")
+    def get_logger(self, logger_name):
+        self.proj_logger = logging.Logger(logger_name)
         self.proj_logger.level = logging.DEBUG
         self.log_file_handler = logging.FileHandler("../logs/default_log_file.log")
         self.log_formatter = logging.Formatter("%(asctime)s : %(name)s : %(message)s")
@@ -21,10 +21,11 @@ class Greenkart_Logging:
         self.proj_logger.addHandler(self.log_file_handler)
         return self.proj_logger
 
-
+"""
 if __name__ == '__main__':
     
     tester = Greenkart_Logging()
     log_obj = tester.get_logger()
     log_obj.info("TESTING IT OUT")
     print("TESTING COMPLETE")    
+"""
--- selenium/rahul_shetty_website1/testcases/test_rahul_shetty_website_1.py
+++ selenium/rahul_shetty_website1/testcases/test_rahul_shetty_website_1.py
@@ -5,64 +5,77 @@ Created on May 6, 2024
 '''
 
 
-
-from selenium import webdriver
+import pytest
 from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
+from pytest_fw.GreenkartLogger import Greenkart_Logging
+from pytest_fw.pytest_fixtures import suite_setup
 
-driver_obj = webdriver.Chrome()
-driver_obj.maximize_window()
-driver_obj.implicitly_wait(5)
 
-expl_wait = WebDriverWait(driver=driver_obj, timeout=15)
+# @pytest.mark.usefixtures(suite_setup)
 
-driver_obj.get("https://rahulshettyacademy.com/seleniumPractise/#/")
-
-driver_obj.find_element(By.XPATH, "//input[@type='search']").send_keys("be")
-time.sleep(5)
-
-veggie_cards = driver_obj.find_elements(By.XPATH, "//div[@class='product']")
-
-for each_veggie in veggie_cards:
-    driver_obj.find_element(By.XPATH, "//button[text()='ADD TO CART']").click()
-
-#Click on the cart
-driver_obj.find_element(By.CLASS_NAME, "cart-icon").click()
-
-driver_obj.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
-time.sleep(5)
-
-# Get total amount in each row in table
-amounts_lst = driver_obj.find_elements(By.XPATH, "//tr/td[5]")
-final_amts_lst = amounts_lst[1:]
-
-basket_total = 0
-for each_total_amt in final_amts_lst:
-    basket_total += int(each_total_amt.text)
-    
-website_basket_total = driver_obj.find_element(By.XPATH, "//span[@class='totAmt']").text
-assert int(website_basket_total) == basket_total
-
-# Now apply the Promocode
-driver_obj.find_element(By.XPATH, "//input[@class='promoCode']").send_keys("rahulshettyacademy")
-driver_obj.find_element(By.XPATH, "//button[text()='Apply']").click()
-
-expl_wait.until(EC.visibility_of_element_located((By.XPATH, "//span[@class='promoInfo']")), "Code Applied Successfully")
-discount = driver_obj.find_element(By.XPATH, "//span[@class='discountPerc']").text
-assert discount == '10%'
-
-driver_obj.find_element(By.XPATH, "//button[text()='Place Order']").click()
-time.sleep(5)
-# Select Country
-drop_down_obj = Select(driver_obj.find_element(By.XPATH, "//Select"))
-drop_down_obj.select_by_visible_text("India")
-
-agree_box = driver_obj.find_element(By.XPATH, "//input[@type='checkbox']")
-agree_box.click()
-
-assert agree_box.is_enabled()
-
-driver_obj.find_element(By.XPATH, "//button[text()='Proceed']").click()
+class Test_GreenKartFirst(suite_setup):
+    def test_e2e(self):
+        driver_obj = self.driver
+        logger_obj = Greenkart_Logging().get_logger(__name__)
+        expl_wait = WebDriverWait(driver=driver_obj, timeout=15)
+        logger_obj.info("Website loaded")
+        
+        driver_obj.find_element(By.XPATH, "//input[@type='search']").send_keys("be")
+        time.sleep(5)
+        logger_obj.info("Website search box found")
+        
+        veggie_cards = driver_obj.find_elements(By.XPATH, "//div[@class='product']")
+        logger_obj.info("Multiple veggie cards found!!")
+        
+        for each_veggie in veggie_cards:
+            driver_obj.find_element(By.XPATH, "//button[text()='ADD TO CART']").click()
+        
+        logger_obj.info("Veggies added to cart")
+        
+        #Click on the cart
+        driver_obj.find_element(By.CLASS_NAME, "cart-icon").click()
+        
+        driver_obj.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
+        time.sleep(5)
+        logger_obj.info("Checking out with veggies in the cart")
+        
+        # Get total amount in each row in table
+        amounts_lst = driver_obj.find_elements(By.XPATH, "//tr/td[5]")
+        final_amts_lst = amounts_lst[1:]
+        
+        basket_total = 0
+        for each_total_amt in final_amts_lst:
+            basket_total += int(each_total_amt.text)
+            
+        website_basket_total = driver_obj.find_element(By.XPATH, "//span[@class='totAmt']").text
+        assert int(website_basket_total) == basket_total
+        logger_obj.info("Assertion Successful. Calculated basket total equal to the one shown on website")
+        
+        # Now apply the Promocode
+        driver_obj.find_element(By.XPATH, "//input[@class='promoCode']").send_keys("rahulshettyacademy")
+        driver_obj.find_element(By.XPATH, "//button[text()='Apply']").click()
+        
+        expl_wait.until(EC.visibility_of_element_located((By.XPATH, "//span[@class='promoInfo']")), "Code Applied Successfully")
+        discount = driver_obj.find_element(By.XPATH, "//span[@class='discountPerc']").text
+        assert discount == '10%'
+        logger_obj.info("Promocode applied successfully")
+        
+        driver_obj.find_element(By.XPATH, "//button[text()='Place Order']").click()
+        time.sleep(5)
+        # Select Country
+        drop_down_obj = Select(driver_obj.find_element(By.XPATH, "//Select"))
+        drop_down_obj.select_by_visible_text("India")
+        logger_obj.info("Country selected")
+        
+        agree_box = driver_obj.find_element(By.XPATH, "//input[@type='checkbox']")
+        agree_box.click()
+        logger_obj.info("Conditions accepted")
+        
+        assert agree_box.is_enabled()
+        
+        driver_obj.find_element(By.XPATH, "//button[text()='Proceed']").click()
+        logger_obj.info("Test case completed successfully")
